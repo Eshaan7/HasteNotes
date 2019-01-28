0bb1b6e6075fbbcb29000c1494cab9a738d032f8
Eshaan Bansal <ishubansal2000@gmail.com>
2019-01-28 21:30:08 +0530

    modified:   static/index.js

--- static/index.js
+++ static/index.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#submit').disabled = true;
     document.querySelector('#new-note').onkeyup = () => {
         if (document.querySelector('#new-note').value.length > 0 ) 
-        document.querySelector('#submit').disabled = false;
-    else
-        document.querySelector('#submit').disabled = true;
+        	document.querySelector('#submit').disabled = false;
+    	else
+        	document.querySelector('#submit').disabled = true;
     };
     document.querySelector('form').onsubmit = () => {
         document.querySelector('#submit').disabled = true;
