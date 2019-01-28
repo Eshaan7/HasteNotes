228716911984beba4836c96beed97268385d78de
Eshaan Bansal <ishubansal2000@gmail.com>
2019-01-28 23:23:53 +0530

    modified:   application.py

--- application.py
+++ application.py
@@ -1,22 +1,24 @@
-
 from flask import Flask, render_template, request
-#from flask_session import Session
 
 app = Flask(__name__)
 
-"""app.config["SESSION_PERMANENT"] = True
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app) """
-
-notes = []
-
 @app.route('/', methods=['GET','POST'])
 def index():
-	n = ''
 	if request.method == 'POST':
+		n = ''
 		n = request.form.get('note')
 		if n:
-			notes.append(n)
+			with open('notes.txt', 'a+') as NotesFileHandle:
+				NotesFileHandle.write("%s\n" % n)
+	notes = []
+	try:
+		NotesFileHandle = open('notes.txt', 'r')
+	except:	
+		NotesFileHandle = open('notes.txt', 'a+')
+		NotesFileHandle.seek(0,0)
+	for cur_note in NotesFileHandle.readlines():
+		notes.append(cur_note.rstrip())
+	NotesFileHandle.close()
 	return render_template('index.html', notes=notes)
 
 if __name__=='__main__': 
