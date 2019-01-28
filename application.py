c623f7235dd2b795148a0c162517d46b02759db4
Eshaan Bansal <ishubansal2000@gmail.com>
2019-01-28 17:32:49 +0530

    removed sessions

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 
-from flask import Flask, render_template, request, session
-from flask_session import Session
+from flask import Flask, render_template, request
+#from flask_session import Session
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = True
+"""app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+Session(app) """
 
 notes = []
 
